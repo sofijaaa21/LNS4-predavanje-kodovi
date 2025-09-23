79c2f9be023018f0b4db5c6ab98f90bea57b2d4d
Sofija Stanojlovic <stanojlovicsofija06@gmail.com>
2025-09-23 10:01:42 +0200

    first commit

---
+++ model_klasifikacije_zadatak1.js
@@ -0,0 +1,76 @@
+// Teachable Machine
+// The Coding Train / Daniel Shiffman
+// https://thecodingtrain.com/TeachableMachine/1-teachable-machine.html
+// https://editor.p5js.org/codingtrain/sketches/PoZXqbu4v
+
+// The video
+let video;
+// For displaying the label
+let label = "waiting...";
+// The classifier
+let classifier;
+// ovde izmenite link do vaseg modela
+let modelURL = 'https://teachablemachine.withgoogle.com/models/WA3z8CfxL/';
+
+
+// STEP 1: Load the model!
+function preload() {
+  classifier = ml5.imageClassifier(modelURL + 'model.json');
+}
+
+
+function setup() {
+  createCanvas(640, 520);
+  // Create the video
+  video = createCapture(VIDEO);
+  video.hide();
+  // STEP 2: Start classifying
+  classifyVideo();
+}
+
+// STEP 2 classify the videeo!
+function classifyVideo() {
+  classifier.classify(video, gotResults);
+}
+
+function draw() {
+  background(0);
+
+  // Draw the video
+  image(video, 0, 0);
+
+  // STEP 4: Draw the label
+  textSize(32);
+  textAlign(CENTER, CENTER);
+  fill(255);
+  text(label, width / 2, height - 16);
+
+  // Pick an naziv,
+  
+  if (label == "ja") {
+    naziv = "ja";
+  } else if (label=="sveska") {
+    naziv = "sveska";
+  }
+  else{
+    naziv="flasa";
+  }
+  
+
+
+  // Draw the naziv
+  textSize(256);
+  text(naziv, width / 2, height / 2);
+}
+
+// STEP 3: Get the classification!
+function gotResults(error, results) {
+  // Something went wrong!
+  if (error) {
+    console.error(error);
+    return;
+  }
+  // Store the label and classify again!
+  label = results[0].label;
+  classifyVideo();
+}
